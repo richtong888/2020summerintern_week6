c6fc5236b5c0bc0ef0ba93fe2c1a38104a05c548
richtong <richtong888@gmail.com>
2020-08-13 17:54:52 +0800

    remove devel build

--- gazebo_ws/catkin_ws/build/ROS_GAZEBO_examples/robot_car_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/richtong/gazebo_ws/catkin_ws/src/ROS_GAZEBO_examples/robot_car_description
-# Build directory: /home/richtong/gazebo_ws/catkin_ws/build/ROS_GAZEBO_examples/robot_car_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- gazebo_ws/catkin_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/richtong/gazebo_ws/catkin_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/richtong/gazebo_ws/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/richtong/gazebo_ws/catkin_ws/devel;/opt/ros/melodic' != '' else []
